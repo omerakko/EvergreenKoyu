69e4f725931ccb46ef91cc0a15c5919ff0955f1b
omerakko <46012036+omerakko@users.noreply.github.com>
2023-04-28 13:51:51 +0200

    my commit

---
+++ scripts/scroll.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("learn-more").style.display = "inline-block";
+    }, 2000); // 2000 milliseconds (2 seconds)
+}
